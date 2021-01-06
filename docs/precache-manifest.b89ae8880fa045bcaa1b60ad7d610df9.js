aeae6edc0544a91199c86a41a17d93463212b20c
Juan Manuel Mauricio Zamarrón <jmauricio@quarksoft.net>
2021-01-06 13:56:41 -0600

    Se agrega carpeta docs

---
+++ docs/precache-manifest.b89ae8880fa045bcaa1b60ad7d610df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682b87950a6264d6d92f9f29a35556c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3bc852c0056c5cf1292",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3bc852c0056c5cf1292",
+    "url": "/static/js/main.a9e534eb.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
